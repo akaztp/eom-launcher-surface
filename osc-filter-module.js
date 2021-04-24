568e0e86690acfa9b86130def1010ac33a68dcfe
José Proença <joseantonioproenca@gmail.com>
2021-04-24 17:35:18 +0100

    Increased message filtering on the filter module.

--- osc-filter-module.js
+++ osc-filter-module.js
@@ -1,8 +1,19 @@
 module.exports = {
     oscInFilter:function(data){
-        var {address} = data
+        var {address, args} = data
         if (address === '/time/str') {
             return undefined;
+        }
+        if (address === '/beat/str') {
+            if (!args[0].value.endsWith('.1')) {
+                return undefined;
+            }
+        }
+        var parts = address.split('/');
+        if (parts[1] === 'track') {
+            if (parts[3] === 'clip' && parts[5] === 'isPlayingQueued') {
+                return undefined
+            }
         }
         return data;
     },
